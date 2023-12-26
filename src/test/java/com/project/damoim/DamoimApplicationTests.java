62156467a5420d904fed14fa111d5c33f2193b08
bumjun2 <jbj3713@naver.com>
2023-12-26 15:49:24 +0900

    # 초기 세팅

---
+++ src/test/java/com/project/damoim/DamoimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.damoim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamoimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
