a01ad13f1c32b382eedb05089e842b2512632f44
GaRyung <shubom0115@naver.com>
2023-12-28 16:56:41 +0900

    오늘 고생했습니다 !!!!!!!

---
+++ src/main/java/com/project/damoim/post/common/Post.java
@@ -0,0 +1,27 @@
+package com.project.damoim.post.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+public class Post {
+
+    private int pageNo; // 클라이언트가 보낸 페이지번호
+    private int amount; // 클라이언트가 보낸 목록게시물 수
+
+
+    public Post() {
+        this.pageNo = 1;
+        this.amount = 6;
+    }
+
+
+    public int getPageStart() {
+        return (pageNo - 1) * amount;
+    }
+}
