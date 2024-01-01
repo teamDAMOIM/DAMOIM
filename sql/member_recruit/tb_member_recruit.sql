9d42332ed897a972e04c55a4ddc8ad054a90800e
oyg9731 <oyg9731@naver.com>
2024-01-01 23:56:10 +0900

    asd

--- sql/member_recruit/tb_member_recruit.sql
+++ sql/member_recruit/tb_member_recruit.sql
@@ -1,5 +1,6 @@
 -- tb_member_recruit 테이블 생성
 
+<<<<<<< Updated upstream
 CREATE TABLE tb_member_recruit(
     member_id VARCHAR(255),
     recruit_no INT,
@@ -8,4 +9,12 @@ CREATE TABLE tb_member_recruit(
     FOREIGN KEY (member_id) REFERENCES tb_member (member_id),
     FOREIGN KEY (recruit_no) REFERENCES tb_recruit (recruit_no)
 );
+=======
+use damoim
+;
+
+create table tb_member(
+    member_id VARCHAR(255),
+    member_password VARCHAR(255) NOT NULL
+>>>>>>> Stashed changes
 
