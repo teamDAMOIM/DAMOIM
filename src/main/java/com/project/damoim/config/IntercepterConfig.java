da7674e2a143a54ac2245f3b27253117b962ca0a
bumjun2 <jbj3713@naver.com>
2024-01-07 15:11:29 +0900

    모집 게시판 회원제 처리

--- src/main/java/com/project/damoim/config/IntercepterConfig.java
+++ src/main/java/com/project/damoim/config/IntercepterConfig.java
@@ -4,6 +4,7 @@ package com.project.damoim.config;
 import com.project.damoim.interceptor.AfterLoginInterceptors;
 import com.project.damoim.interceptor.AutoLoginInterceptors;
 import com.project.damoim.interceptor.PostDetailInterceptors;
+import com.project.damoim.interceptor.RecruitDetailInterceptors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,6 +17,7 @@ public class IntercepterConfig implements WebMvcConfigurer {
     private final AfterLoginInterceptors afterLoginInterceptors;
     private final AutoLoginInterceptors autoLoginInterceptors;
     private final PostDetailInterceptors postDetailInterceptors;
+    private final RecruitDetailInterceptors recruitDetailInterceptors;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
@@ -32,6 +34,16 @@ public class IntercepterConfig implements WebMvcConfigurer {
         registry
                 .addInterceptor(postDetailInterceptors)
                 .addPathPatterns("/post/detail", "/post/write")
+        ;
+
+        registry
+                .addInterceptor(postDetailInterceptors)
+                .addPathPatterns("/post/detail", "/post/write")
+        ;
+
+        registry
+                .addInterceptor(recruitDetailInterceptors)
+                .addPathPatterns("/recruit/detail", "/recruit/write")
         ;
     }
 }
