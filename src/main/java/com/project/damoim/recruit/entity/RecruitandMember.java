1ede1f2e2a5b79361938052da4be8eb7f0c4ee58
bumjun2 <jbj3713@naver.com>
2024-01-08 01:39:13 +0900

    no

--- src/main/java/com/project/damoim/recruit/entity/RecruitandMember.java
+++ src/main/java/com/project/damoim/recruit/entity/RecruitandMember.java
@@ -3,6 +3,8 @@ package com.project.damoim.recruit.entity;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @ToString
@@ -14,5 +16,7 @@ public class RecruitandMember {
     private String memberId;
     private int recruitNo;
     private int ischeck;
-
+    private String recruitTitle;
+    private LocalDateTime recruitStartdate;
+    private LocalDateTime recruitEnddate;
 }
